11a6eef8af86e3dfbaf42d8916763be46d3bc207
John Wang <john29917958@gmail.com>
2015-12-19 20:10:43 +0800

    Create test case for DynamicObjectModule.

---
+++ src/tests/dynamicObjectModule/DynamicObjectModuleTest.java
@@ -0,0 +1,79 @@
+package tests.dynamicObjectModule;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class DynamicObjectModuleTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testDynamicObjectModule() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testAddVirtualCharacter() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFindItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testFindVirtualCharacter() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetAllDynamicObjects() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testGetVirtualCharacterPosition() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testKeyGETPressed() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUpdateItem() {
+		fail("Not yet implemented"); // TODO
+	}
+
+	@Test
+	public void testUpdateVirtualCharacter() {
+		fail("Not yet implemented"); // TODO
+	}
+
+}
